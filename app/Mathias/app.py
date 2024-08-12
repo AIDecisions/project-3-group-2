76422200d895535dd2dc0d7fb9802b67a0f3b21d
Carnero <carnero@andrews.edu>
2024-08-11 21:05:16 -0400

    Updates

--- app/Mathias/app.py
+++ app/Mathias/app.py
@@ -1,93 +1,82 @@
-import numpy as np
-import pandas as pd
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify, render_template
-
-
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("sqlite:///Resources/allattacks.sqlite")
-conn = engine.connect()
-# reflect an existing database into a new model
-# Base = automap_base()
-# # reflect the tables
-# Base.prepare(autoload_with=engine)
-
-# # Save reference to the table
-# Combined_attacks = Base.classes.combined_attacks
-#print(Base.classes.keys())
-
+from sqlHelper import SQLHelper
+# from flask_cors import CORS
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
-
+sql = SQLHelper()
+#CORS(app)
 #################################################
 # Flask Routes
 #################################################
 
-@app.route("/")
-def index():
+
+@app.route('/')
+def welcome():
     return render_template('index.html')
+    # return ('''
+    #         Welcome to the Climate Analysis API!                                                                                                                                            <br/>
+    #         Available Routes:                                                                                                                                                               <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/full_data_sql">/api/v1.0/full_data_sql</a>                                                                                           <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/over_time/&lt;animal_type&gt;/&lt;sex&gt;">/api/v1.0/over_time/&lt;animal_type&gt;/&lt;sex&gt;</a>                                   <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/main_places/&lt;animal_type&gt;/&lt;sex&gt;">/api/v1.0/main_places/&lt;animal_type&gt;/&lt;sex&gt;</a>                               <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/map_places/&lt;animal_type&gt;/&lt;sex&gt;">/api/v1.0/map_places/&lt;animal_type&gt;/&lt;sex&gt;</a>                                 <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/injuries/&lt;animal_type&gt;/&lt;sex&gt;">/api/v1.0/injuries/&lt;animal_type&gt;/&lt;sex&gt;</a>                                     <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;<a href="/api/v1.0/table_data">/api/v1.0/table_data/&lt;animal_type&gt;/&lt;sex&gt;</a>                                                                 <br/>
+    #                                                                                                                                                                                         <br/>
+    #                                                                                                                                                                                         <br/>
+    #         Explanation of the Routes:                                                                                                                                                      <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;full_data_sql: Returns all the data from the table using SQL.                                                                                           <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;over_time/&lt;animal_type&gt;/&lt;sex&gt;: Returns the number of attacks over time for a specific animal                                                <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;main_places/&lt;animal_type&gt;/&lt;sex&gt;: Returns the top 20 main places where the attacks happened for a specific animal.                           <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;map_places/&lt;animal_type&gt;/&lt;sex&gt;: Returns the places where the attacks happened for a specific animal.                                        <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;injuries/&lt;animal_type&gt;/&lt;sex&gt;: Returns the injuries that happened for a specific animal.                                                     <br/>
+    #         &nbsp;&nbsp;&nbsp;&nbsp;table_data/&lt;animal_type&gt;/&lt;sex&gt;: Returns the data for the table chart.                                                                       <br/>
+    #         ''')
 
 
-@app.route("/api/v1.0/names")
-def names():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all attack names"""
-    # Query all attacks
-    results = session.query(attack.name).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
-
-    return jsonify(all_names)
+@app.route("/api/v1.0/full_data_sql")
+def full_data_sql():
+    data = sql.full_data_sql()
+    return (jsonify(data))
 
 
-@app.route("/api/v1.0/attacks_by_gender")
-def attacks_by_gender():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # """Return a list of attack data including the name, age, and sex of each attack"""
-    # # Query all attacks
-    # results = session.query(Combined_attacks.year,Combined_attacks.sex).all()
-    query = """SELECT * 
-        FROM combined_attacks"""
-    df = pd.read_sql(query, conn)
-    # print(df.head())
-
-    session.close()
-
-    results = df[['year','sex']].copy()
-    results.dropna(inplace = True)
-    results['year'] = results['year'].astype(int)
-
-    results = results.value_counts().reset_index().sort_values('year')
-    results.columns = ['year', 'sex', 'attacks']
+@app.route("/api/v1.0/over_time/<animal_type>/<sex>")
+def over_time(animal_type, sex):
+    data = sql.over_time(animal_type, sex)
+    return (jsonify(data))
 
 
-    # Create a dictionary from the row data and append to a list of all_attacks
-    all_attacks = []
-    for index, row in results.iterrows():
-        attack_dict = {}
-        attack_dict["year"] = row["year"]
-        attack_dict["sex"] = row["sex"]
-        attack_dict["attacks"] = row["attacks"]
-        all_attacks.append(attack_dict)
-
-    return jsonify(all_attacks)
+@app.route("/api/v1.0/main_places/<animal_type>/<sex>")
+def main_places(animal_type, sex):
+    data = sql.main_places(animal_type, sex)
+    return (jsonify(data))
 
 
+@app.route("/api/v1.0/map_places/<animal_type>/<sex>")
+def map_places(animal_type, sex):
+    data = sql.map_places(animal_type, sex)
+    return (jsonify(data))
+
+
+@app.route("/api/v1.0/injuries/<animal_type>/<sex>")
+def injuries(animal_type, sex):
+    data = sql.injuries(animal_type, sex)
+    return (jsonify(data))
+
+
+@app.route("/api/v1.0/table_data/<animal_type>/<sex>")
+def table_data(animal_type, sex):
+    data = sql.table_data(animal_type, sex)
+    return (jsonify(data))
+
+@app.route("/api/v1.0/get_line")
+def get_line():
+    line_data = sql.get_line()
+   
+    return (jsonify(line_data))
+
+# Run the App
 if __name__ == '__main__':
     app.run(debug=True)
--- app/Mathias/static/js/app.js
+++ app/Mathias/static/js/app.js
@@ -1,17 +1,114 @@
-// d3.json("http://127.0.0.1:5000/api/v1.0/attacks_by_gender")
-//   .then(function(data){
-//     // Code from your callback goes here...
-//     console.log(data)
-//   });
-  function updateChart(animalType) {
-    // selected animal type
-    buildCharts(animalType)
-    d3.json("https://www.kaggle.com/datasets/danela/fatal-bear-attacks-north-america").then((data) => {
-        // Get the metadata field
-        console.log("This is bear attacks data",data)
-        //let metadata = data.metadata;
+console.log("app.js isloaded");
+function make_table() {
+    // Get filter requests from the form
+    var animal_filter = d3.select("#animal_filter").property("value");
+    var gender_filter = d3.select("#gender_filter").property("value");
+    console.log("Animal Filter: " + animal_filter);
+    console.log("Gender Filter: " + gender_filter);
+
+    // Make a request to the table data route
+    let url = "/api/v1.0/table_data/" + animal_filter + "/" + gender_filter;
+    console.log(url);
+
+    // Make a request to the table data route
+    d3.json(url).then(function(data) {
+        console.log(data);
+        // Get a reference to the table body
+        var tbody = d3.select("data_table");
+
+        // Loop through data and append to table
+        data.forEach((data) => {
+            var row = tbody.append("tr");
+            Object.entries(data).forEach(([key, value]) => {
+                var cell = row.append("td");
+                cell.text(value);
+            });
+        });
     });
-  }
-  function buildCharts(animalType) {
-    // selected animal type
-  }
+
+  // line graph funtion MC
+  make_line()
+}
+ 
+// line graph funtion MC
+async function make_line() {
+const url = "/api/v1.0/get_line"
+const response = await d3.json(url)
+console.log("make_line:", response);
+ 
+var trace1 = {
+  x: response.Year,
+  y: response.Number of Attacks,
+  mode: 'markers',
+  type: 'line',
+  name: 'Team A',
+  // text: ['A-1', 'A-2', 'A-3', 'A-4', ],
+  marker: { size: 7 }
+};
+ 
+var data = [ trace1, ];
+ 
+var layout = {
+  // xaxis: {
+  //   range: [ 0.50, 5.25 ]
+  // },
+  // yaxis: {
+  //   range: [0, 8]
+  // },
+  title:'Area Chart of Attack Counts Over Time'
+};
+ 
+Plotly.newPlot('line_chart', data, layout);
+}
+
+    // Make a request to the table data route
+    // d3.json(url).then(function(data) {
+    //     console.log(data);
+        // // Get a reference to the table body
+        // var tbody = d3.select("data_table");
+
+        // // Loop through data and append to table
+        // data.forEach((data) => {
+        //     var row = tbody.append("tr");
+        //     Object.entries(data).forEach(([key, value]) => {
+        //         var cell = row.append("td");
+        //         cell.text(value);
+        //     });
+        // });
+    // });  
+}
+
+
+//     // Make a request to the table data route
+//     d3.json("/api/v1.0/table_data/").then((data) => {
+//     // Get a reference to the table body
+//     var tbody = d3.select("tbody");
+
+//     // Loop through data and append to table
+//     data.forEach((data) => {
+//         var row = tbody.append("tr");
+//         Object.entries(data).forEach(([key, value]) => {
+//             var cell = row.append("td");
+//             cell.text(value);
+//         });
+//     });
+// });
+
+
+// Initialize the page with a default plot
+function init() {
+    // console.log("Initializing page...");
+    // Create charts
+    make_table();
+}
+
+function updateChart() {
+    make_table();
+    
+}
+    
+// // Event listener for the filter button
+// d3.select("#filter").on("click", init());
+
+// Initialize the page with a default plot
+init()
